da6d064fbc16fe88cb273913f0df6995191b2d9d
A01661436 <102175249+A01661436@users.noreply.github.com>
2023-04-24 22:50:54 -0600

    Delete Tarea3 directory

--- Tarea3/frontend/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path, include
-from . import views
-
-urlpatterns = [
-    path('', views.index, name="index"),
-    path('aboutus', views.aboutus, name="aboutus"),
-    path('chart', views.chart, name="chart"),
-    path('log', views.log, name="log")
-]
